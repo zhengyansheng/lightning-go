28ac9dbe65c030cbb3b2db5fdb00d2c645c1c488
kcloud <kcloud@kclouds-Mac-mini.local>
2021-04-25 11:45:06 +0800

    add cron

--- internal/cron/index.go
+++ internal/cron/index.go
@@ -1,1 +1,15 @@
 package cron
+
+import (
+	"github.com/douyu/jupiter/pkg/worker/xcron"
+)
+
+func InitShedule(cron *xcron.Cron) {
+	//add cron task here
+
+	// ex
+	//if !conf.GetBool("kjcloud.disable_iso") {
+	//	cron.Schedule(xcron.Every(time.Minute*5), xcron.FuncJob(ResetInstallStatus))
+	//}
+
+}
